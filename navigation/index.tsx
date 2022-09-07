81f7d1282ce8c85197b559398a51816344c29771
알파 <arpa1705@gmail.com>
2022-09-07 15:19:39 +0900

    feat: 홈 스크린 컴포넌트 추가

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -90,21 +90,6 @@ function BottomTabNavigator() {
         options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
           title: "홈",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate("Modal")}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}
-            >
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
         })}
       />
       <BottomTab.Screen
